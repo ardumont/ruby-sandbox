c14eac4052c39718b8a735bff01e6a3e71a4a1c2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-15 16:33:14 +0200

    Refacto.

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -19,18 +19,18 @@ def triangle(a, b, c)
                    raise TriangleError.new("Side of a triangle cannot be zero or negative!")
                  end
   }
- if a+b>c && a+c>b && b+c>a
-    # the passing case
-    if a == b && b == c
-      return :equilateral
-    elsif a == b || a == c || b == c
-      return :isosceles
-    else
-      return :scalene
-    end
- else
-  raise TriangleError.new("The sum of 2 sides must be superior or equal to the third side!")
- end
+  if a+b<=c || a+c<=b || b+c<=a
+    raise TriangleError.new("The sum of 2 sides must be superior or equal to the third side!")
+  end
+
+  # the passing case
+  if a == b && b == c
+    return :equilateral
+  elsif a == b || a == c || b == c
+    return :isosceles
+  else
+    return :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.

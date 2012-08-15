337cf387f1750093d1f0e5364eac1f62177483b9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-15 10:15:08 +0200

    Begin the implementation of the mega greeter.

---
+++ mega-greeter.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+class MegaGreeter
+  attr_accessor :names
+
+  def initialize (names="World")
+    @names = names
+  end
+
+  # say hi to everybody
+  # if name is nil then hello world!
+  # else for each elements in the list, then hello to all
+  # else hello to just the one person
+  def hi
+    if @names.nil?
+      put "Hello World!"
+    else if @names.respond_to?("each")
+      @names.each do |name|
+          puts "Hello #{name}"
+      end
+    else
+      puts "Hello #{names}"
+    end
+  end
+end
+
+
